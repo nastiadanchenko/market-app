99b4e852ae8cce30d7e98caf7ee0d3c8a7b2a3eb
Nastia Danchenko <nastiadanchenko@gmail.com>
2025-11-09 20:45:26 +0500

    feat: init app

---
+++ src/main/java/yandex/workshop/market/MarketAppApplication.java
@@ -0,0 +1,13 @@
+package yandex.workshop.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketAppApplication.class, args);
+    }
+
+}
