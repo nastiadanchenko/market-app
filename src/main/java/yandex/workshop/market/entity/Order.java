90cb35ced487feed78ff081f2769c2ba44105b90
Nastia Danchenko <nastiadanchenko@gmail.com>
2025-11-29 20:40:13 +0500

    feat(entity): update entity for reactive interaction

--- src/main/java/yandex/workshop/market/entity/Order.java
+++ src/main/java/yandex/workshop/market/entity/Order.java
@@ -1,31 +1,18 @@
 package yandex.workshop.market.entity;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.SequenceGenerator;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name = "orders")
+@Table(name = "orders")
 public class Order {
     @Id
-    @SequenceGenerator(name = "order_sequence", sequenceName = "order_sequence", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "order_sequence")
     private Long id;
-
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
-    private List<OrderItem> items;
-
     private Long totalSum;
 
 }
