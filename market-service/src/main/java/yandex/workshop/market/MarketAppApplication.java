e788f5fe6d14e4e27f85d7880c5f39f0d632cd97
Nastia Danchenko <nastiadanchenko@gmail.com>
2025-12-13 22:03:11 +0500

    feat(ApiController): create interactions with payment-service module

--- market-service/src/main/java/yandex/workshop/market/MarketAppApplication.java
+++ market-service/src/main/java/yandex/workshop/market/MarketAppApplication.java
@@ -2,10 +2,16 @@ package yandex.workshop.market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import yandex.workshop.market.api.DefaultApi;
 
 @SpringBootApplication
 public class MarketAppApplication {
 
+    @Bean
+    public DefaultApi getDefaultApi(){
+        return new DefaultApi();
+    }
     public static void main(String[] args) {
         SpringApplication.run(MarketAppApplication.class, args);
     }
