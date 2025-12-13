d33e6fcc5f54fa42eb7c46f1aff5310b3476ab4d
Nastia Danchenko <nastiadanchenko@gmail.com>
2025-12-13 22:04:05 +0500

    test: edit ApiControllerTest

--- market-service/src/test/java/yandex/workshop/market/ApiControllerTest.java
+++ market-service/src/test/java/yandex/workshop/market/ApiControllerTest.java
@@ -13,12 +13,16 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.transaction.reactive.TransactionalOperator;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import yandex.workshop.market.api.DefaultApi;
 import yandex.workshop.market.controller.ApiController;
+import yandex.workshop.market.domain.BalanceResponse;
+import yandex.workshop.market.domain.PaymentResponse;
 import yandex.workshop.market.dto.Action;
 import yandex.workshop.market.dto.ItemDto;
 import yandex.workshop.market.dto.ItemsPageDto;
@@ -27,8 +31,9 @@ import yandex.workshop.market.dto.PagingDto;
 import yandex.workshop.market.dto.mapperDto.ItemMapper;
 import yandex.workshop.market.service.ItemService;
 import yandex.workshop.market.service.OrderService;
+import yandex.workshop.market.service.PaymentService;
 
-@WebFluxTest(ApiController.class)
+@WebFluxTest({ApiController.class, DefaultApi.class})
 public class ApiControllerTest {
     @MockitoBean
     private ConnectionFactory connectionFactory;
@@ -44,6 +49,9 @@ public class ApiControllerTest {
 
     @MockitoBean
     private OrderService orderService;
+
+    @MockitoBean
+    private PaymentService paymentService;
 
     private ItemDto itemDto;
 
@@ -142,12 +150,19 @@ public class ApiControllerTest {
     @Test
     @DisplayName("GET /cart/items - должен возвращать страницу с товарами в корзине")
     void shouldReturnCartItemsPage() throws Exception {
-//        ItemDto itemDto = new ItemDto(1L, "Title", BigDecimal.valueOf(100), 2, "/img.png", "Desc");
+        BalanceResponse balanceResponse = new BalanceResponse();
+        balanceResponse.setBalance(500.00);
+
+        ResponseEntity<BalanceResponse> responseEntity =
+            ResponseEntity.ok(balanceResponse);
 
         Mockito.when(itemService.findItemsDtoByCountGreaterThanZero())
             .thenReturn(Flux.just(itemDto));
         Mockito.when(itemService.getTotalSum())
             .thenReturn(Mono.just(200L));
+
+        Mockito.when(paymentService.getBalance())
+            .thenReturn(Mono.just(500.0));
 
         webTestClient.get()
             .uri("/cart/items")
@@ -155,8 +170,12 @@ public class ApiControllerTest {
             .expectStatus().isOk()
             .expectBody(String.class)
             .consumeWith(r -> {
-                assert r.getResponseBody().contains("cart");
-                assert r.getResponseBody().contains("200");
+                String body = r.getResponseBody();
+                assert body != null;
+                assert body.contains("cart");
+                assert body.contains("200");
+                assert !body.contains("Недостаточно средств");
+                assert !body.contains("Платежная услуга недоступна");
             });
     }
 
@@ -216,15 +235,30 @@ public class ApiControllerTest {
 
     @Test
     @DisplayName("POST /buy  - должен создавать заказ и перенаправлять на страницу заказа")
-    void shouldCreateOrderAndRedirect() throws Exception {
+    void shouldCreateOrderAndRedirect() {
+        long total = 100L;
+
         OrderDto orderDto = new OrderDto(1L, List.of(), 100L);
+
         Mockito.when(orderService.createOrder())
             .thenReturn(Mono.just(orderDto));
 
+        PaymentResponse paymentResponse = new PaymentResponse();
+        ResponseEntity<PaymentResponse> paymentEntity =
+            ResponseEntity.ok(paymentResponse);
+
+        Mockito.when(itemService.getTotalSum())
+            .thenReturn(Mono.just(total));
+
+        Mockito.when(paymentService.pay(any()))
+            .thenReturn(Mono.just(ResponseEntity.ok(new PaymentResponse())));
+
+        // when / then
         webTestClient.post()
             .uri("/buy")
             .exchange()
-            .expectStatus().is3xxRedirection();
+            .expectStatus().is3xxRedirection()
+            .expectHeader().valueEquals("Location", "/orders/1");
     }
 
 
